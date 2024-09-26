d61ba621510b82fc69e2266f4bf9924cdb786fc1
Asota Randy <randy201312@gmail.com>
2024-09-26 23:10:00 +0300

    Car, Sedan : перенес классы, добавил функционал

---
+++ lessons/oop/abstraction/Sedan.java
@@ -0,0 +1,41 @@
+package lessons.oop.abstraction;
+
+// Класс для примеров. Наследование
+// Реализация абстрактного класса Car
+public class Sedan extends Car {
+    String cheapVersion;
+
+    public static void main(String[] args) {
+        Sedan bmw = new Sedan("BMW", 320, "dark blue");
+        bmw.cheapVersion = "Stage 1";
+
+        bmw.gas();
+        bmw.turnSignal();
+    }
+
+    // метод-конструктор
+    public Sedan(String model, int maxSpeed, String color) {
+        this.setModel(model);
+        this.setMaxSpeed(maxSpeed);
+        this.setColor(color);
+    }
+
+    // аннотация, говорящая компилятору и том что происходит переопределение или (в данном случае) реализация метода
+    @Override
+    public void gas(){
+        System.out.println("Полный вперед!");
+    }
+
+    @Override
+    public void turnSignal(){
+        if(this.getModel().equals("BMW")){
+            System.out.println("Прости, что?");
+        } else {
+            System.out.println("Включил поворотник");
+        }
+    }
+
+    public void showCheapVersion(){
+        System.out.println("Версия чип-тюнинга двигателя: " + this.cheapVersion);
+    }
+}
